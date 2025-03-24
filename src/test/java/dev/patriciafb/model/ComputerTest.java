4ced09c9b41e031da285765cf5d31082614b9e10
PFBregon <bregon@gmail.com>
2025-03-24 22:54:31 +0100

    testCreateComputer created and failed

---
+++ src/test/java/dev/patriciafb/model/ComputerTest.java
@@ -0,0 +1,26 @@
+package dev.patriciafb.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ComputerTest {
+
+    @Test
+    void tesCreateComputer() {
+        String brand = "Dell";
+        int memory = 16;
+        String cpu = "Intel Core i7";
+        String os = "Windows 10";
+        double price = 699;
+
+        Computer computer = new Computer(brand, memory, cpu, os, price);
+
+        assertThat(computador.getMarca(), is(marca));
+        assertThat(computador.getMemoria(), is(memoria));
+        assertThat(computador.getProcesador(), is(procesador));
+        assertThat(computador.getSistemaOperativo(), is(sistemaOperativo));
+        assertThat(computador.getPrecio(), closeTo(precio, 0.01));
+    }
+
+
+}
