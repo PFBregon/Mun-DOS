7d4cd44e2514c18047169758b5b13d2b2c4d5659
PFBregon <bregon@gmail.com>
2025-03-25 13:18:45 +0100

    TestListAllComputers run ok. Store and storeTest are ok

--- src/main/java/dev/patriciafb/model/Store.java
+++ src/main/java/dev/patriciafb/model/Store.java
@@ -5,46 +5,48 @@ import java.util.List;
 
 public class Store {
 
-        private String storeName;
-        private String owner;
-        private String taxId;
-        private List<Computer> computers;
+    private String storeName;
+    private String owner;
+    private String taxId;
+    private List<Computer> computers;
 
-        public Store(String storeName, String owner, String taxId) {
-            this.storeName = storeName;
-            this.owner = owner;
-            this.taxId = taxId;
-            this.computers = new ArrayList<>();
-        }
+    public Store(String storeName, String owner, String taxId) {
+        this.storeName = storeName;
+        this.owner = owner;
+        this.taxId = taxId;
+        this.computers = new ArrayList<>();
+    }
 
-        public String getStoreName() {
-            return storeName;
-        }
+    public String getStoreName() {
+        return storeName;
+    }
 
-        public String getOwner() {
-            return owner;
-        }
+    public String getOwner() {
+        return owner;
+    }
 
-        public String taxId() {
-            return taxId;
-        }
+    public String taxId() {
+        return taxId;
+    }
 
-        public void addComputer(Computer computer) {
-            computers.add(computer);
-        }
+    public void addComputer(Computer computer) {
+        computers.add(computer);
+    }
 
-        public boolean removeComputerByBrand(String string) {
-            return computers.removeIf(computer -> computer.getBrand().equals(string));
-        }
+    public boolean removeComputerByBrand(String string) {
+        return computers.removeIf(computer -> computer.getBrand().equals(string));
+    }
 
-        public List<Computer> searchComputerByBrand(String brand) {
-            List<Computer> result = new ArrayList<>();
-            for (Computer computer : computers) {
-                if (computer.getBrand().equals(brand)) {
-                    result.add(computer);
-                }
+    public List<Computer> searchComputerByBrand(String brand) {
+        List<Computer> result = new ArrayList<>();
+        for (Computer computer : computers) {
+            if (computer.getBrand().equals(brand)) {
+                result.add(computer);
             }
+        }
             return result;
         }
-
-    }
+    public List<Computer> listAllComputers() {
+        return computers;
+        }
+}
