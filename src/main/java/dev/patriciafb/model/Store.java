e0adb0f8e8b25074e6c1c553fc245a228fb1ba4f
PFBregon <bregon@gmail.com>
2025-03-25 12:20:50 +0100

    testAddComputer created and failed

--- src/main/java/dev/patriciafb/model/Store.java
+++ src/main/java/dev/patriciafb/model/Store.java
@@ -1,21 +1,45 @@
 package dev.patriciafb.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
 
-    public Store(String string, String string2, String string3) {
-        //TODO Auto-generated constructor stub
+        private String storeName;
+        private String owner;
+        private String taxId;
+        private List<Computer> computers;
+
+
+        public Store(String storeName, String owner, String taxId) {
+            this.storeName = storeName;
+            this.owner = owner;
+            this.taxId = taxId;
+            this.computers = new ArrayList<>();
+        }
+
+
+        public String getStoreName() {
+            return storeName;
+        }
+
+        public String getOwner() {
+            return owner;
+        }
+
+        public String taxId() {
+            return taxId;
+
+        }
+
+
+        public void addComputer(Computer computer) {
+            throw new UnsupportedOperationException("Unimplemented method 'addComputer'");
+        }
+
+
+        public List<Computer> listAllComputers() {
+            throw new UnsupportedOperationException("Unimplemented method 'listAllComputers'");
+        }
     }
 
-    public void addComputer(Computer computer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addComputer'");
-    }
-
-    public List<Computer> listAllComputers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listAllComputers'");
-    }
-
-}
